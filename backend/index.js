f4e520a64cb4e6ed5f0dc7efed72de1e7439cda5
KartikMouli <kartikmouli0@gmail.com>
2024-09-11 16:10:07 +0530

    try_7

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 app.use(cors({
-    origin: "*",
+    origin: process.env.CLIENT_URL,
     credentials: true
 }))
 
@@ -170,9 +170,9 @@ app.use((err, req, res, next) => {
 })
 
 
-app.use(express.static(path.join(__dirname, "../client")))
+app.use(express.static(path.join(__dirname,"../client")))
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "index.html"))
+    res.sendFile(path.join(__dirname,"../client","index.html"))
 })
 
 app.listen(port, () => {
